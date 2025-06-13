7e0dd8bf94e0ce24a7e5752869d0da8248cc480a
Rigi <regina.zaets@guild-of-developers.ru>
2025-06-13 13:16:27 +0300

    [+] добавила хук форм, изменения на странице входа, есть валидация полей

---
+++ src/items/input/Input.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react';
+import s from './Input.module.scss';
+import clsx from 'clsx';
+
+type InputProps = {
+  label: string;
+  placeholder: string;
+  type: 'email' | 'password';
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
+  error?: boolean;
+};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, placeholder, type, onChange, onBlur, error = false, ...rest }, ref) => {
+    return (
+      <div className={s.field}>
+        <p className={s.subTitle}>{label}</p>
+        <input
+          type={type}
+          placeholder={placeholder}
+          ref={ref}
+          {...rest}
+          className={clsx(s.input, { [s.input__error]: error })}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      </div>
+    );
+  },
+);
