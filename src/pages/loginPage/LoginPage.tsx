126a2858c95045fc3be9e7b2dfb25260ad4d71c0
Rigi <regina.zaets@guild-of-developers.ru>
2025-06-14 11:02:43 +0300

    [~] немного исправила позиционирование контейнеров и изменила стили/картинку

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,15 +1,15 @@
 import s from './LoginPage.module.scss';
-import task from '../../public/images/task1.png';
+import man from '../../public/images/man.png';
 import { Login } from '../../components/login/Login';
 export const LoginPage = () => {
   return (
     <div className={s.container}>
       <div className={s.main}>
         <div className={s.left}>
-          <img src={task} alt='tasks' />
+          <img src={man} alt='man_with_laptop' />
         </div>
         <div className={s.right}>
-          <Login/>
+          <Login />
         </div>
       </div>
     </div>
